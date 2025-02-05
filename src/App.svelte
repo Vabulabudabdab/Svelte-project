0d8d96ca484e8cef32fbc1fafa88a17c8fce7264
Vabulabudabdab <Sarkisowa7@gmail.com>
2025-02-05 19:42:58 +0300

    first commit message

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { Router, Link, Route } from "svelte-routing";
+  import Register from "./routes/Register.svelte";
+  import Login from "./routes/Login.svelte";
+
+
+  export let url = "";
+</script>
+
+<main>
+
+</main>
+
+<Router url="{url}">
+
+  <div>
+    <Route path="/register"><Register /></Route>
+    <Route path="/login"><Login /></Route>
+  </div>
+
+
+</Router>
